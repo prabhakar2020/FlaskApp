5a3ee6cc68a43563ed32bb86570f61de4eb77b59
prabhakar <prabhakar2020@gmail.com>
2019-10-31 12:19:37 +0530

    modified response text

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! Welcome, This is sample flask application"
+    return "Hello World! This is sample first flask application"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=9999)
