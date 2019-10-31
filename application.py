7898a24c661a94304ee6a82f1e1bfd036c3443d0
prabhakar <prabhakar2020@gmail.com>
2019-10-31 12:17:41 +0530

    modified port

--- application.py
+++ application.py
@@ -6,4 +6,4 @@ def hello():
     return "Hello World! Welcome, This is sample flask application"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=5000,debug=True)
+    app.run(host='0.0.0.0',port=9999)
